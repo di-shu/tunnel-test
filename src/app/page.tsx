d638467fc1c58ba3793647a3dbb45519553d4516
di-shu <54156812+di-shu@users.noreply.github.com>
2024-02-06 13:36:36 +0200

    update app_env to be staging and use it for TunnelToolbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { TunnelToolbar } from "@tunnel/nextjs";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {(process.env.NODE_ENV !== "development") && (
+      {(process.env.APP_ENV === "staging") && (
 
         <TunnelToolbar
           projectId={process.env.TUNNEL_PROJECT_ID}
