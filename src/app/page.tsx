31817235e6f0a3ee02112214c11bc3c32c64a067
di-shu <54156812+di-shu@users.noreply.github.com>
2024-02-08 01:33:24 +0200

    move Tunnel to common layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { TunnelToolbar } from "@tunnel/nextjs";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {(process.env.APP_ENV === "staging") && (
-
-        <TunnelToolbar
-          projectId={process.env.TUNNEL_PROJECT_ID || 'id'}
-          branch="main"
-        />
-      )}
+      Home
     </main>
   );
 }
